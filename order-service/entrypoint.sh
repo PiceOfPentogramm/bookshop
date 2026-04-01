3c98496a907658ad8af85a83182ddbb9622a7c1d
PiceOfPentogramm <piceofpentogramm@gmail.com>
2026-04-01 17:59:45 +0300

    add prometheus and grafana

--- order-service/entrypoint.sh
+++ order-service/entrypoint.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 until nc -z postgres 5432; do sleep 1; done
+until nc -z user-service 8001; do sleep 1; done
+until nc -z book-service 8002; do sleep 1; done
+until nc -z rabbitmq 5672; do sleep 1; done
 alembic upgrade head
 uvicorn main:app --host 0.0.0.0 --port 8003
---
+++ postgres/init.sql
@@ -0,0 +1,4 @@
+CREATE DATABASE bookshop_users;
+CREATE DATABASE bookshop_books;
+CREATE DATABASE bookshop_orders;
+CREATE DATABASE bookshop_notifications;
